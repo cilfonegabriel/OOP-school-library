ccc624cae39abaace6568c95b4e3a51bec5efcb7
gabicilfone <cilfonegabriel@gmail.com>
2023-03-13 14:33:22 -0300

    add main file for testing and fixes

---
+++ library/person.rb
@@ -0,0 +1,24 @@
+class Person
+  def initialize(age, name = 'unknown', parent_permission: true)
+    @id = id
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
+
+  # Getters
+  attr_reader :id
+  attr_accessor :name, :age
+  attr_reader :parent_permission
+
+  # Setters
+  def can_use_services?
+    of_age? || parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
