0b64e4cb6e7e7d0aa7c7a93176c6f94352a2c17e
gabicilfone <cilfonegabriel@gmail.com>
2023-03-13 17:12:07 -0300

    add trimmer decorator

---
+++ library/trimmer_decorator.rb
@@ -0,0 +1,14 @@
+require_relative 'base_decorator'
+class TrimmerDecorator < BaseDecorator
+    attr_accessor :nameable
+
+    def initialize(nameable)
+        @nameable = nameable
+        super()
+    end
+
+
+    def correct_name
+        nameable.correct_name[0...10]
+    end
+end
