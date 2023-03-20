64f80243a6a8204fb0436afaae885d08cb3db8f8
gabicilfone <cilfonegabriel@gmail.com>
2023-03-20 16:02:27 -0300

    fix linters

--- library/modules/4_add_person.rb
+++ library/modules/4_add_person.rb
@@ -1,26 +1,26 @@
 module AddPerson
-    def create_student
-        print('Name: ')
-        name = gets.chomp
-        print('Age: ')
-        age = gets.chomp
-        print('Has parent permission [Y/N]: ')
-        permission = gets.chomp
-        permission = permission != ('n' || 'N')
-        new_student = Student.new(age, nil, name, parent_permission: permission)
-        @people.push(new_student)
-        puts 'Student created successfully'
-    end
+  def create_student
+    print('Name: ')
+    name = gets.chomp
+    print('Age: ')
+    age = gets.chomp
+    print('Has parent permission [Y/N]: ')
+    permission = gets.chomp
+    permission = permission != ('n' || 'N')
+    new_student = Student.new(age, nil, name, parent_permission: permission)
+    @people.push(new_student)
+    puts 'Student created successfully'
+  end
 
-    def create_teacher
-        print('Name: ')
-        name = gets.chomp
-        print('Age: ')
-        age = gets.chomp
-        print('Specialization: ')
-        spec = gets.chomp
-        new_teacher = Teacher.new(age, spec, name)
-        @people.push(new_teacher)
-        puts 'Teacher created successfully'
-      end
+  def create_teacher
+    print('Name: ')
+    name = gets.chomp
+    print('Age: ')
+    age = gets.chomp
+    print('Specialization: ')
+    spec = gets.chomp
+    new_teacher = Teacher.new(age, spec, name)
+    @people.push(new_teacher)
+    puts 'Teacher created successfully'
+  end
 end
