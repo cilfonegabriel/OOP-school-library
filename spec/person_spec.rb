98e26d627b3bf44d4ffc9b7fcc62bfa20d95693d
gabicilfone <cilfonegabriel@gmail.com>
2023-03-22 15:59:32 -0300

    add test for person

---
+++ spec/person_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../modules/person'
+
+RSpec.describe Person do
+  let(:person) { Person.new(18, '1234-5678', 'John Smith', parent_permission: true) }
+  let(:book) { double('Book') }
+  
+  describe '#can_use_services?' do
+    context 'when the person is of age' do
+      it 'returns true' do
+        expect(person.can_use_services?).to be true
+      end
+    end
+  end
+  
+  describe '#to_json' do
+    it 'returns a JSON representation of the person' do
+      expect(person.to_json).to eq({
+        JSON.create_id => 'Person',
+        'id' => '1234-5678',
+        'name' => 'John Smith',
+        'age' => 18,
+        'parent_permission' => true
+      }.to_json)
+    end
+  end
+end
