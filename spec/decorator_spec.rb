c062bdf7cacc00442e98b9b1034f2344bbe1a5e7
gabicilfone <cilfonegabriel@gmail.com>
2023-03-22 20:14:58 -0300

    add unit test for decorator and person

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,13 +1,21 @@
 require_relative './../modules/decorator'
 require_relative './../modules/nameable'
 
-describe '#new' do
-  decorator = Decorator.new('label')
-  it 'takes a new decorator object' do
-    expect(decorator).to be_instance_of(Decorator)
+describe Decorator do
+  describe '#initialize' do
+    it 'sets the nameable instance variable' do
+      nameable = double('nameable')
+      decorator = Decorator.new(nameable)
+      expect(decorator.instance_variable_get(:@nameable)).to eq(nameable)
+    end
   end
 
-  it 'returns a nameable object' do
-    expect(decorator).to be_kind_of(Nameable)
+  describe '#correct_name' do
+    it 'calls correct_name on the nameable object' do
+      nameable = double('nameable')
+      decorator = Decorator.new(nameable)
+      expect(nameable).to receive(:correct_name)
+      decorator.correct_name
+    end
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,26 +1,71 @@
 require_relative '../modules/person'
 
-RSpec.describe Person do
-  let(:person) { Person.new(18, '1234-5678', 'John Smith', parent_permission: true) }
-  let(:book) { double('Book') }
+describe Person do
+  let(:person) { Person.new(20) }
+  let(:book) { double('book') }
 
-  describe '#can_use_services?' do
-    context 'when the person is of age' do
-      it 'returns true' do
-        expect(person.can_use_services?).to be true
-      end
+  describe '#initialize' do
+    it 'sets the age attribute' do
+      expect(person.age).to eq(20)
+    end
+
+    it 'sets the id attribute with a unique identifier' do
+      expect(person.id).not_to be_nil
+    end
+
+    it 'sets the name attribute with a default value' do
+      expect(person.name).to eq('Unknown')
+    end
+
+    it 'sets the parent_permission attribute with a default value' do
+      expect(person.parent_permission).to be true
+    end
+
+    it 'initializes the rentals attribute as an empty array' do
+      expect(person.rentals).to be_empty
     end
   end
 
   describe '#to_json' do
-    it 'returns a JSON representation of the person' do
-      expect(person.to_json).to eq({
+    it 'returns a JSON string representation of the Person object' do
+      json = {
         JSON.create_id => 'Person',
-        'id' => '1234-5678',
-        'name' => 'John Smith',
-        'age' => 18,
+        'id' => person.id,
+        'name' => 'Unknown',
+        'age' => 20,
         'parent_permission' => true
-      }.to_json)
+      }.to_json
+      expect(person.to_json).to eq(json)
+    end
+  end
+
+  describe '#can_use_services?' do
+    context 'when the person is 18 years or older' do
+      it 'returns true' do
+        expect(person.can_use_services?).to be true
+      end
+    end
+
+    context 'when the person is under 18 years old and has parental permission' do
+      let(:person) { Person.new(16, parent_permission: true) }
+
+      it 'returns true' do
+        expect(person.can_use_services?).to be true
+      end
+    end
+
+    context 'when the person is under 18 years old and does not have parental permission' do
+      let(:person) { Person.new(16, parent_permission: false) }
+
+      it 'returns false' do
+        expect(person.can_use_services?).to be false
+      end
+    end
+  end
+
+  describe '#correct_name' do
+    it 'returns the name attribute' do
+      expect(person.correct_name).to eq('Unknown')
     end
   end
 end
