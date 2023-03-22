188cff85c6938ecc3b9e6fed085f4d35839c5e63
gabicilfone <cilfonegabriel@gmail.com>
2023-03-22 16:09:14 -0300

    add test for capitalize decorate

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,13 @@
+require_relative './../modules/capitalize_decorator'
+require_relative './../modules/person'
+
+describe CapitalizeDecorator do
+    let(:nameable) { double('nameable', correct_name: 'john doe') }
+    subject { CapitalizeDecorator.new(nameable) }
+  
+    describe '#correct_name' do
+      it 'capitalizes the correct name' do
+        expect(subject.correct_name).to eq('John doe')
+      end
+    end
+  end
