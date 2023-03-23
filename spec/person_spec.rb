1a1f514626039c4960f4959c5e9a98efc5c3c611
gabicilfone <cilfonegabriel@gmail.com>
2023-03-23 08:33:24 -0300

    add fix unit test for person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,71 +1,38 @@
 require_relative '../modules/person'
+require_relative '../modules/book'
+require_relative '../modules/rental'
 
 describe Person do
-  let(:person) { Person.new(20) }
-  let(:book) { double('book') }
+  let(:person) { Person.new(20, '1234', 'John Doe') }
+  let(:book) { Book.new('The Great Gatsby', 'F. Scott Fitzgerald') }
 
-  describe '#initialize' do
-    it 'sets the age attribute' do
-      expect(person.age).to eq(20)
+  describe '#of_age?' do
+    it 'returns true if the person is 18 or over' do
+      expect(person.send(:of_age?)).to eq true
     end
 
-    it 'sets the id attribute with a unique identifier' do
-      expect(person.id).not_to be_nil
-    end
-
-    it 'sets the name attribute with a default value' do
-      expect(person.name).to eq('Unknown')
-    end
-
-    it 'sets the parent_permission attribute with a default value' do
-      expect(person.parent_permission).to be true
-    end
-
-    it 'initializes the rentals attribute as an empty array' do
-      expect(person.rentals).to be_empty
-    end
-  end
-
-  describe '#to_json' do
-    it 'returns a JSON string representation of the Person object' do
-      json = {
-        JSON.create_id => 'Person',
-        'id' => person.id,
-        'name' => 'Unknown',
-        'age' => 20,
-        'parent_permission' => true
-      }.to_json
-      expect(person.to_json).to eq(json)
-    end
-  end
-
-  describe '#can_use_services?' do
-    context 'when the person is 18 years or older' do
-      it 'returns true' do
-        expect(person.can_use_services?).to be true
-      end
-    end
-
-    context 'when the person is under 18 years old and has parental permission' do
-      let(:person) { Person.new(16, parent_permission: true) }
-
-      it 'returns true' do
-        expect(person.can_use_services?).to be true
-      end
-    end
-
-    context 'when the person is under 18 years old and does not have parental permission' do
-      let(:person) { Person.new(16, parent_permission: false) }
-
-      it 'returns false' do
-        expect(person.can_use_services?).to be false
-      end
+    it 'returns false if the person is under 18' do
+      person.age = 16
+      expect(person.send(:of_age?)).to eq false
     end
   end
 
   describe '#correct_name' do
-    it 'returns the name attribute' do
-      expect(person.correct_name).to eq('Unknown')
+    it 'returns the person\'s name' do
+      expect(person.correct_name).to eq 'John Doe'
+    end
+  end
+
+  describe '#add_rental' do
+    it 'adds a rental to the book\'s rentals array' do
+      expect {
+        person.add_rental(book)
+      }.to change(book.rentals, :size).by(1)
+    end
+
+    it 'creates a new Rental object with the person and book' do
+      expect(Rental).to receive(:new).with(Date.today, person, book)
+      person.add_rental(book)
     end
   end
 end
